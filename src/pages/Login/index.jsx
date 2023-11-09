c53627b148de05354dd5b36dbc158d9dbd5dac4e
lawrencenoman-01 <PhinconTraineeOlen@01>
2023-11-09 14:52:23 +0700

    Design and Modification Structure App

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <div> Haloo </div>
+    </div>
+  )
+}
+
+export default Login
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,11 @@
 import MainLayout from '@layouts/MainLayout';
+import Detail from '@pages/Detail';
+import Form from '@pages/Form';
 
 import Home from '@pages/Home';
+import Login from '@pages/Login';
 import NotFound from '@pages/NotFound';
+import Register from '@pages/Register';
 
 const routes = [
   {
@@ -9,6 +13,34 @@ const routes = [
     name: 'Home',
     protected: false,
     component: Home,
+    layout: MainLayout,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    protected: false,
+    component: Login,
+    layout: MainLayout,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    protected: false,
+    component: Register,
+    layout: MainLayout,
+  },
+  {
+    path: '/detail/:id',
+    name: 'Detail',
+    protected: false,
+    component: Detail,
+    layout: MainLayout,
+  },
+  {
+    path: '/form',
+    name: 'Form',
+    protected: false,
+    component: Form,
     layout: MainLayout,
   },
   { path: '*', name: 'Not Found', component: NotFound, layout: MainLayout, protected: false },
