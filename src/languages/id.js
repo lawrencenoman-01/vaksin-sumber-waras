825ceeafcf2ad282267b393ad1bee7e608105655
lawrencenoman-01 <PhinconTraineeOlen@01>
2023-11-10 15:01:05 +0700

    Features add

--- src/languages/id.js
+++ src/languages/id.js
@@ -7,4 +7,14 @@ export default {
   app_popup_error_title: 'Terjadi kesalahan!',
   app_popup_error_message: 'Maaf, telah terjadi kesalahan. Silakan coba beberapa saat lagi',
   app_popup_close_button_label: 'Tutup',
+
+  // Style Languange Olen
+  app_signup: 'MULAI PENDAFTARAN',
+  app_signup_name: 'nama',
+  app_signup_email: 'Alamat Email',
+  app_signup_password: 'Kata Kunci',
+
+  app_login: 'SELAMAT DATANG',
+  app_login_email: 'Alamat Email',
+  app_login_password: 'Kata Kunci',
 };
--- src/reducers.js
+++ src/reducers.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 
 import appReducer, { storedKey as storedAppState } from '@containers/App/reducer';
 import clientReducer, { storedKey as storedClientState } from '@containers/Client/reducer';
+import registerReducer from '@pages/Register/reducer';
+import loginReducer, { storedKey as storedLoginState } from '@pages/Login/reducer';
 import languageReducer from '@containers/Language/reducer';
 
 import { mapWithPersistor } from './persistence';
@@ -9,6 +11,8 @@ import { mapWithPersistor } from './persistence';
 const storedReducers = {
   app: { reducer: appReducer, whitelist: storedAppState },
   client: { reducer: clientReducer, whitelist: storedClientState },
+  register: { reducer: registerReducer },
+  login: { reducer: loginReducer, whitelist: storedLoginState },
 };
 
 const temporaryReducers = {
